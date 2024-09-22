5d3c29e9a7befebf253bddc527d6cdbb0a284d3b
hamid heydari <hamid6885@gmail.com>
2024-09-23 02:42:00 +0330

    ci: extend nuxt default root directory and home page routing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from 'path'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  // Define Source Directory
+  srcDir: './client',
+
+  // Extend Default Routing
+  hooks: {
+    'pages:extend'(pages) {
+      pages.push({
+        name: 'home',
+        path: '/',
+        file: resolve(__dirname, 'client/pages/home/index.vue')
+      })
+    }
+  }
 })
