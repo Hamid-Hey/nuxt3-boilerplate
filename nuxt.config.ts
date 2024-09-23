caeac0f39d550ecb407925bde48729bfeb3579ed
hamid heydari <hamid6885@gmail.com>
2024-09-24 02:26:29 +0330

    ci: config sass and sass loader

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
 
+  // Global CSS
+  css: ['~/assets/styles/main.scss'],
+
   // Define Source Directory
   srcDir: './client',
 
